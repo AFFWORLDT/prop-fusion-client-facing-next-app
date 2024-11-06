14a3fc7f6d787ad80773ecd870b77dd303f86ee1
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-11-06 16:42:50 +0530

    feat(auth): add authentication check with user data response
    - Integrated Clerk's `auth` and `currentUser` functions for authentication
    - Added GET endpoint that returns "Unauthenticated" with 401 status if no userId is found
    - Returns "Authenticated" message with userId and username when user is authenticated

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,20 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+export async function GET() {
+  const { userId } = await auth();
+  const user = await currentUser();
+  if (!userId) {
+    return NextResponse.json(
+      {
+        message: "Unauthenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  return NextResponse.json({
+    message: "Authenticated",
+    data: { userId, username: user?.username },
+  });
+}
