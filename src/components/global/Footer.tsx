36e40dac5aab8c077dcae355e4bbf54dbc12fd3f
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-11-08 19:51:16 +0530

    fix : add  issue at srollToTop

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+import Image from "next/image";
 import React from "react";
 
 import {
@@ -9,20 +11,15 @@ import {
   FaMobile,
   FaFax,
   FaArrowUp,
-  FaMoon,
-  FaSun,
 } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
-import prop7 from "../public/images/prop7.jpg";
-import prop8 from "../public/images/prop8.jpg";
-import Image from "next/image";
-import Link from "next/link";
 
 const Footer = () => {
   return (
     <>
       <footer
-        className={`bg-gray-800 w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`}>
+        className={`bg-gray-800 w-full m-auto lg:px-20 px-10 py-20 grid lg:grid-cols-3 grid-cols-1 justify-center items-start lg:gap-20 gap-10`}
+      >
         <div className="flex flex-col justify-center items-start gap-5">
           <h1 className="text-white text-2xl font-semibold">About Us</h1>
           <p className="text-slate-200 text-justify">
@@ -32,7 +29,8 @@ const Footer = () => {
           </p>
           <div
             id="social-icons"
-            className="flex justify-start items-center gap-4 mt-4">
+            className="flex justify-start items-center gap-4 mt-4"
+          >
             <div className="p-3 rounded-xl bg-white hover:bg-red-600 hover:text-white cursor-pointer transform hover:scale-110 transition-transform duration-300">
               <FaFacebookF className="size-5" />
             </div>
@@ -78,46 +76,52 @@ const Footer = () => {
         </div>
 
         <div className="flex flex-col justify-center items-start gap-5">
-  <h1 className="text-white text-2xl font-semibold">
-    Latest Properties
-  </h1>
+          <h1 className="text-white text-2xl font-semibold">
+            Latest Properties
+          </h1>
 
-  {/* Property 1 */}
-  <div className="flex justify-start items-center gap-4">
-    <img
-      src={"/images/prop7.jpg"}
-      alt="Property Image 1"
-      className="w-[120px] rounded-lg transform hover:scale-110 cursor-pointer transition-transform duration-300"
-    />
-    <div>
-      <h1 className="text-lg text-white">Villa with amazing view</h1>
-      <p className="text-slate-400">$ 287.98</p>
-    </div>
-  </div>
+          {/* Property 1 */}
+          <div className="flex justify-start items-center gap-4">
+            <Image
+              width={120}
+              height={120}
+              src={"/images/prop7.jpg"}
+              alt="Property Image 1"
+              className="w-[120px] rounded-lg transform hover:scale-110 cursor-pointer transition-transform duration-300"
+            />
+            <div>
+              <h1 className="text-lg text-white">Villa with amazing view</h1>
+              <p className="text-slate-400">$ 287.98</p>
+            </div>
+          </div>
 
-  {/* Property 2 */}
-  <div className="flex justify-start items-center gap-4">
-    <img
-      src={"/images/prop8.jpg"}
-      alt="Property Image 2"
-      className="w-[120px] rounded-lg transform hover:scale-110 cursor-pointer transition-transform duration-300"
-    />
-    <div>
-      <h1 className="text-lg text-white">Smart View on beach</h1>
-      <p className="text-slate-400">$ 467.18</p>
-    </div>
-  </div>
-</div>
-
+          {/* Property 2 */}
+          <div className="flex justify-start items-center gap-4">
+            <Image
+              width={120}
+              height={120}
+              src={"/images/prop8.jpg"}
+              alt="Property Image 2"
+              className="w-[120px] rounded-lg transform hover:scale-110 cursor-pointer transition-transform duration-300"
+            />
+            <div>
+              <h1 className="text-lg text-white">Smart View on beach</h1>
+              <p className="text-slate-400">$ 467.18</p>
+            </div>
+          </div>
+        </div>
       </footer>
 
       {/* slide to top button */}
       <div
         id="icon-box"
-        className="bg-red-600 p-4 rounded-full hover:bg-black cursor-pointer fixed lg:bottom-12 bottom-6 right-6">
-        <Link href="#hero" smooth={true} offset={-100} duration={500}>
-          <FaArrowUp className="size-6 text-white" />
-        </Link>
+        className="bg-red-600 p-4 rounded-full hover:bg-black cursor-pointer fixed lg:bottom-12 bottom-6 right-6"
+        onClick={() => {
+          const element = document.getElementById("hero");
+          element?.scrollIntoView({ behavior: "smooth" });
+        }}
+      >
+        <FaArrowUp className="size-6 text-white" />
       </div>
     </>
   );
